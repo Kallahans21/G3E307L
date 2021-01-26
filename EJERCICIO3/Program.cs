e09d1f96abfbaa3e54c33edd221f0ca9b0848545
KallahanS21 <andyy.salasb@gmail.com>
2021-01-26 08:09:04 -0600

    EJERCICIO3

---
+++ EJERCICIO3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EJERCICIO3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido. Vamos a realizar la ecuación x= 4/3πr^3 , Para comenzar:");
+            Console.WriteLine("Digite el valor del radio: ");
+            //Declaración de variables y proceso de datos
+            Double r, x;
+            r = Double.Parse(Console.ReadLine());
+            x = ((4 * 3.1415926554) / 3) * Math.Pow(Convert.ToDouble(r), 3);
+
+            //salida de datos
+            Console.WriteLine("El resultado de X en esta ecuación, es de: " + x);
+            Console.ReadKey();
+
+        }
+    }
+}
